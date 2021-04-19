17dd3fedbf8503ae16bf150d2b1457ae04451260
DS-87 <79628525+DS-87@users.noreply.github.com>
2021-04-19 23:48:16 +0900

    Update 초보자를위한피이썬300제.py

--- 초보자를위한피이썬300제.py
+++ 초보자를위한피이썬300제.py
@@ -1,5 +1,42 @@
 print("Hello World")
 print("Mar's cosmetics")
 print('신씨가 소리질렀다. "도둑이야".')
+print('"C:\Windows"')
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+print("오늘은","월요일")
+print("naver","kakao","sk","samsung",sep=";")
+print("naver","kakao","sk","samsung",sep="/")
+print("first",end="");print("second")
+print(5/3)
+Samsung=50000
+account=Samsung*10
+print(account)
+시가총액=298000000000000
+현재가=50000
+PER=15.79
+print(시가총액, type(시가총액))
+print(현재가, type(현재가))
+print(PER, type(PER))
 
-print('"C:\Windows"')c
+s="hello"
+t="python"
+print(s+"!",t)
+
+a = "132"
+print(type(a))
+문자열 str
+
+a= 132
+
+print(type(a))
+정수 int
+
+num_str="720"
+num_int=int(num_str)
+
+에어컨을 월 48,584원에 무이자 36개월로 구매할 경우
+월=48584
+납부일=36
+총비용=월*납부일
+print(총비용)
+
